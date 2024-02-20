5ab922d3120ff1943ab697e106b3aa0676cfb8a1
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 15:20:02 +0530

    Update translation.py

--- plugins/translation.py
+++ plugins/translation.py
@@ -11,9 +11,44 @@ I Am Telegram TOOLS Bot.
 
 **Use Help Button To Know How To Use Me**
 """
+    HELP_TEXT = """
+𒊹︎︎︎ How To Upload File Or Media 
+
+➪ Send Your Link For Upload File Or Media.
+
+𒊹︎︎︎ How to set thumbnail
+
+➪ Send Your Thumbnail Photo And Permanent Added Your Photo.
+
+𒊹︎︎︎ How To Deleting Thumbnail
+
+➪ Send /delthumb To Delete Your Thumbnail.
+
+𒊹︎︎︎ How To Show Thumbnail 
+
+➪ Send /showthumb To View Custom Thumbnail 
+➪ How to use bot to avoid ban ❗❗ **[check out](https://telegra.ph/HOW-TO-AVOID-BAN-12-14)**
+ 
+"""
+    ABOUT_TEXT = """
+**📛 My Name** : [Url xV2🚀](http://t.me/URLX_bot)
+
+**❤️ Version** : [2 beta 🔥](http://t.me/URLX_bot)
+
+**🤖 Source** : [Click](https://github.com)
+
+**🧿 Language** : [Python 3.10.11](https://www.python.org/)
+
+**📢 Framework** : [Pyrogram 1.4.16](https://docs.pyrogram.org/)
+
+**👨‍💻 Developer** : [nothing](https://t.me/shado_hackers)
+
+**IMPORTANT** [HOW TO USE](https://telegra.ph/HOW-TO-AVOID-BAN-12-14)
+
+"""
  START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('text=f"Projects Channel url="https://t.me/omg_info"')
+        InlineKeyboardButton('Settings', callback_data='OpenSettings')
         ],[
         InlineKeyboardButton('Help', callback_data='help'),
         InlineKeyboardButton('About', callback_data='about')
@@ -23,3 +58,19 @@ I Am Telegram TOOLS Bot.
  ) 
  
 
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Back', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about')
+        ],[
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Back', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help')
+        ],[
+        InlineKeyboardButton('🔒 Close', callback_data='close')
+        ]]
+    )
