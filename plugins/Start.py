fcad4e415ababf3886ba60af5c83ca13b4f38b93
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 13:17:57 +0530

    Update Start.py

--- plugins/Start.py
+++ plugins/Start.py
@@ -15,19 +15,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
-    if not update.from_user:
-        return await update.reply_text("I don't know about you sar :(")
- #   await add_user_to_database(bot, update)
-    await bot.send_message(
-     #   Config.LOG_CHANNEL,
-        #   f"NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
-    )
-    
-    #if Config.UPDATES_CHANNEL:
-    #  fsub = await handle_force_subscribe(Client, update)
-   #   if fsub == 400:
-  #      return
-    await update.reply_text(
+      await update.reply_text(
         text=Translation.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
         reply_markup=Translation.START_BUTTONS
