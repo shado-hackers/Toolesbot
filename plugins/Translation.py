e9c123498867000b7f87092b1aa0741a949e3cdf
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 12:07:00 +0530

    Create Translation.py

---
+++ plugins/Translation.py
@@ -0,0 +1,30 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+class Translation(object):
+
+    START_TEXT = """
+🤗 Hello {}
+
+I Am Telegram TOOLS Bot.
+
+**__Send me a direct link and I will upload it to telegram as a file/video.__**
+
+**Use Help Button To Know How To Use Me**
+"""
+  
+ ABOUT_TEXT = """
+**📛 My Name** : [Url xV2🚀](http://t.me/URLX_bot)
+
+**❤️ Version** : [2 beta 🔥](http://t.me/URLX_bot)
+
+**🤖 Source** : [Click](https://github.com)
+
+**🧿 Language** : [Python 3.10.11](https://www.python.org/)
+
+**📢 Framework** : [Pyrogram 1.4.16](https://docs.pyrogram.org/)
+
+**👨‍💻 Developer** : [nothing](https://t.me/shado_hackers)
+
+**IMPORTANT** [HOW TO USE](https://telegra.ph/HOW-TO-AVOID-BAN-12-14)
+
+""" 
