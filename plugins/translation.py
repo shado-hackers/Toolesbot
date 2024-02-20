900ba45d64b0788faff7bc610739e1f4d8a13282
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 15:54:30 +0530

    Update translation.py

--- plugins/translation.py
+++ plugins/translation.py
@@ -7,8 +7,41 @@ class Translation(object):
 
 I Am Telegram TOOLS Bot.
 
-**__Send me a direct link and I will upload it to telegram as a file/video.__**
+**__I AM A TELEGRAM tools bot check help menu for more information create by @shado_hackers__**
 
 **Use Help Button To Know How To Use Me**
 """
+    HELP_TEXT = """
+𒊹︎︎︎ I AM A TOOL BOT 
+➪ check below help menu 
+𒊹︎︎︎ /calendar check calendar
+
+➪ /carbon  to create carbon 
+𒊹︎︎︎ /imdb check on imdb 
+
+➪ /json check json 
+
+𒊹︎︎︎ How To Show Thumbnail 
+
+➪ Send /showthumb To View Custom Thumbnail 
+➪ How to use bot to avoid ban ❗❗ **[check out](https://telegra.ph/HOW-TO-AVOID-BAN-12-14)**
+ 
+"""
+    ABOUT_TEXT = """
+**📛 My Name** : [Url xV2🚀](http://t.me/URLX_bot)
+
+**❤️ Version** : [2 beta 🔥](http://t.me/URLX_bot)
+
+**🤖 Source** : [Click](https://github.com)
+
+**🧿 Language** : [Python 3.10.11](https://www.python.org/)
+
+**📢 Framework** : [Pyrogram 1.4.16](https://docs.pyrogram.org/)
+
+**👨‍💻 Developer** : [nothing](https://t.me/shado_hackers)
+
+
+"""
+    
+    
     
