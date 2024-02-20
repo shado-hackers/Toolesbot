74a47dc26c7f210d8cd3be2ae4fe1a532f22ed04
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 12:16:15 +0530

    Update translation.py

--- plugins/translation.py
+++ plugins/translation.py
@@ -12,22 +12,7 @@ I Am Telegram TOOLS Bot.
 **Use Help Button To Know How To Use Me**
 """
   
- ABOUT_TEXT = """
-**📛 My Name** : [Url xV2🚀](http://t.me/URLX_bot)
-
-**❤️ Version** : [2 beta 🔥](http://t.me/URLX_bot)
-
-**🤖 Source** : [Click](https://github.com)
-
-**🧿 Language** : [Python 3.10.11](https://www.python.org/)
-
-**📢 Framework** : [Pyrogram 1.4.16](https://docs.pyrogram.org/)
-
-**👨‍💻 Developer** : [nothing](https://t.me/shado_hackers)
-
-**IMPORTANT** [HOW TO USE](https://telegra.ph/HOW-TO-AVOID-BAN-12-14)
-
-""" 
+ 
 START_BUTTONS = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton('Settings', callback_data='OpenSettings')
@@ -38,19 +23,4 @@ START_BUTTONS = InlineKeyboardMarkup(
         InlineKeyboardButton('Close', callback_data='close')
         ]]
     )
-    HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Back', callback_data='home'),
-        InlineKeyboardButton('About', callback_data='about')
-        ],[
-        InlineKeyboardButton('Close', callback_data='close')
-        ]]
-    )
-    ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Back', callback_data='home'),
-        InlineKeyboardButton('Help', callback_data='help')
-        ],[
-        InlineKeyboardButton('🔒 Close', callback_data='close')
-        ]]
-    )
+    
