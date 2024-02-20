6d477f6d60b43436e0a29c95689630ac12e74bfc
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 12:10:04 +0530

    Update Start.py

--- plugins/Start.py
+++ plugins/Start.py
@@ -1,15 +1,23 @@
-from pyrogram import *
-import requests as re
-from Config import *
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
-import wget
-import os 
+import os
+import time
+import psutil
+import shutil
+import string
+import asyncio
+from pyrogram import Client, filters
+from asyncio import TimeoutError
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
+from plugins.config import Config
+from plugins.translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     if not update.from_user:
         return await update.reply_text("I don't know about you sar :(")
-    await add_user_to_database(bot, update)
+ #   await add_user_to_database(bot, update)
     await bot.send_message(
      #   Config.LOG_CHANNEL,
         #   f"NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
