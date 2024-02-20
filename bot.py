0f307cec56ca9e71e6e5b6f42cc9134e9a2271ca
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-20 10:40:56 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+from plugins.config import Config
+
+from pyrogram import Client as xbot
+from pyrogram import filters
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+if __name__ == "__main__"  
+    plugins = dict(root="plugins")
+    xbot = xbot(
+        "tools Bot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins)
+    xbot.run()
