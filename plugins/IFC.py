26d0f0d700fd2d0202eb3111458688e94d7e051e
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-22 12:03:36 +0530

    Update IFC.py

--- plugins/IFC.py
+++ plugins/IFC.py
@@ -1,48 +1,38 @@
-from pyrogram import *
-import requests as req
-#from plugins.info import head,ibase_url
-from pyrogram import Client as app
-from pyrogram import filters
-import urllib3
+import asyncio
 
-urllib3.disable_warnings()
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+# Create a Pyrogram client.
+#client = Client("my_bot", api_id=123456, api_hash="0123456789abcdef")
 
-API = "https://ifsc.rizad.me/?ifsc="
-head="**Detailed InFo**\n...................\n\n"
+# Define the command handler.
+@client.on_message(filters.command("ifsc"))
+async def ifsc_handler(client: Client, message: Message):
+    # Get the IFSC code from the message.
+    ifsc_code = message.text.split()[1]
 
-@app.on_message(filters.command("ifsc"))
-async def ifsc_data(client,message):
-    
-   query=message.text.upper()
-   try:
-    http = urllib3.PoolManager()
-    url_request=requests.get( API+query)
-    url_json=url_request.data.decode('utf-8')
-    url_json=json.loads(url_json)
-    
-    #datas
-    swift='Swift :   '+str(url_json['SWIFT'])+'\n'
-    city='City :   '+str(url_json['CITY'])+'\n'
-    upi='UPI :   '+str(url_json['UPI'])+'\n'
-    iso='ISO3166 :   '+str(url_json['ISO3166'])+'\n'
-    neft='NEFT :   '+str(url_json['NEFT'])+'\n'
-    imps='IMPS :   '+str(url_json['IMPS'])+'\n'
-    rtgs='RTGS :   '+str(url_json['RTGS'])+'\n'
-    centre='Centre :   '+str(url_json['CENTRE'])+'\n'
-    address='Address :   '+str(url_json['ADDRESS'])+'\n\n'
-    branch='Branch :   '+str(url_json['BRANCH'])+'\n'
-    micr='MICR :   '+str(url_json['MICR'])+'\n'
-    contact='Contact :   '+str(url_json['CONTACT'])+'\n'
-    dist='District :   '+str(url_json['DISTRICT'])+'\n'
-    state='State :   '+str(url_json['STATE'])+'\n'
-    bank='Bank :   '+str(url_json['BANK'])+'\n'
-    bankcd='Bank Code :   '+str(url_json['BANKCODE'])+'\n'
-    ifsc='IFSC :   '+str(url_json['IFSC'])+'\n'
-    
-    result=head+''+bank+bankcd+ifsc+micr+state+dist+city+branch+address+contact+upi+iso+neft+imps+rtgs+swift+''
-        
-    await message.reply(result)
-   except:
-       await message.reply("Sorry ,'"+query+"' is Invalid IFSC Code 😕")
-       await message.reply("if you're facing a error or else ping @shado_hackers 🤝")
+    # Make a request to the IFSC API.
+    response = await asyncio.get(f"https://ifsc.razorpay.com/{ifsc_code}")
+
+    # Parse the response.
+    data = response.json()
+
+    # Format the response.
+    response_text = f"""
+Bank: {data['BANK']}
+Address: {data['ADDRESS']}
+City: {data['CITY']}
+State: {data['STATE']}
+Branch: {data['BRANCH']}
+MICR Code: {data['MICR']}
+UPI: {data['UPI']}
+RTGS: {data['RTGS']}
+NEFT: {data['NEFT']}
+IMPS: {data['IMPS']}
+"""
+
+    # Send the response to the user.
+    await message.reply_text(response_text)
+
+# Start the bot.
