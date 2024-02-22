1b56e08bc52edc758263359dc7311b1e90ecd7bf
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-22 12:10:13 +0530

    Update IFC.py

--- plugins/IFC.py
+++ plugins/IFC.py
@@ -1,25 +1,25 @@
 import asyncio
-
+import requests
+import json
+ 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-# Create a Pyrogram client.
-#client = Client("my_bot", api_id=123456, api_hash="0123456789abcdef")
-
-# Define the command handler.
 @Client.on_message(filters.command("ifsc"))
 async def ifsc_handler(client: Client, message: Message):
-    # Get the IFSC code from the message.
-    ifsc_code = message.text.split()[1]
+    # Check if the message contains an IFSC code.
+    if len(message.text) > 1:
+        # Get the IFSC code from the message.
+        ifsc_code = message.text.split()[1]
 
-    # Make a request to the IFSC API.
-    response = await asyncio.get(f"https://ifsc.razorpay.com/{ifsc_code}")
+        # Make a request to the IFSC API.
+        response = await asyncio.get(f"https://ifsc.razorpay.com/{ifsc_code}")
 
-    # Parse the response.
-    data = response.json()
+        # Parse the response.
+        data = response.json()
 
-    # Format the response.
-    response_text = f"""
+        # Format the response.
+        response_text = f"""
 Bank: {data['BANK']}
 Address: {data['ADDRESS']}
 City: {data['CITY']}
@@ -32,7 +32,10 @@ NEFT: {data['NEFT']}
 IMPS: {data['IMPS']}
 """
 
-    # Send the response to the user.
-    await message.reply_text(response_text)
-
-# Start the bot.
+        # Send the response to the user.
+        await message.reply_text(response_text)
+    else:
+        # If the message does not contain an IFSC code, send an error message.
+        await message.reply_text("Please provide an IFSC code.")
+ 
+ 
