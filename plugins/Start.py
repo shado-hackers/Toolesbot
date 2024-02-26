fe09f27ed3dc9a887e79e5b040823d9964e78689
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-26 11:16:42 +0530

    Update Start.py

--- plugins/Start.py
+++ plugins/Start.py
@@ -6,19 +6,44 @@ import string
 import asyncio
 from pyrogram import Client, filters
 from asyncio import TimeoutError
+from plugins.config import LOG_CHANNEL
+from plugins.database import db
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
 from plugins.config import Config
 from plugins.translation import Translation
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+    Message
+)
+LOG_TEXT = """<b>#NewUser
+    
+ID - <code>{}</code>
 
+Nᴀᴍᴇ - {}</b>
+"""
 @Client.on_message(filters.command(["start"]))
-async def start(bot, update):
-      await update.reply_text(
-        text=Translation.START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-     )
+async def start_message(c,m):
+    await db.is_user_exist(m.from_user.id)
+    await db.add_user(m.from_user.id, m.from_user.first_name)
+    await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
+    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[OMG INFO](https://t.me/OMG_INFO)**",
+        reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+                    ],  
+                    [
+                        InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'),
+                        InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
+                    ]
+                ]
+            )
+        )
+  
 @Client.on_message(filters.command(["help"]))
 async def help(bot, update):
       await update.reply_text(
