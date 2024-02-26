dfbd0d8b6dde5d0e6d8594aecc3f795fb7266dd4
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-26 16:14:51 +0530

    Update gpt.py

--- plugins/gpt.py
+++ plugins/gpt.py
@@ -37,4 +37,4 @@ async def ai_answer(client, message):
                 
             except Exception as error:
                 print(error)
-                await message.reply_text(f"**An error occurred:**\n\n**{error}**\n\n**Forward This Message To @KingVJ0
+                await message.reply_text(f"**An error occurred:**\n\n**{error}**\n\n**Forward This Message To @KingVJ0")
