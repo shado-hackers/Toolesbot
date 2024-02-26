04bdf2a8b6b3ff02751ed09310d2e06745fe2d6b
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-26 15:54:40 +0530

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -1,6 +1,9 @@
 import re
 from os import environ
 
+import re
+from os import getenv, environ
+
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
