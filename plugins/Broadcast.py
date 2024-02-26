1cb9548bbf16b731c43fc1f787e8d462aaa444f0
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-26 11:44:51 +0530

    Update Broadcast.py

--- plugins/Broadcast.py
+++ plugins/Broadcast.py
@@ -1,7 +1,7 @@
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from plugins.database import db
 from pyrogram import Client, filters
-from plugins.info import ADMINS
+from plugins.config import OWNER_ID
 import asyncio
 import datetime
 import time
@@ -32,7 +32,7 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
 
 
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+@Client.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & filters.reply)
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
