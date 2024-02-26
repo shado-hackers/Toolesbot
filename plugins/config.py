7b6c55e8f83bd9b8217567ad6d6ff424f0e709e2
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-26 11:08:18 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,6 +1,17 @@
 import os
 from os import environ
+import re
+from os import getenv, environ
 
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+        
 import logging
 
 logging.basicConfig(
@@ -45,5 +56,9 @@ class Config(object):
 
     base_url='https://v6.exchangerate-api.com/v6/'+API_KEY+'/latest/'
     base_currency='INR'
-
+    ADMINS = int(environ.get("ADMINS", ""))
+    DB_URI = environ.get("DB_URI", "")
+    DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
+    OPENAI_API = environ.get("OPENAI_API", "")
+    AI = is_enabled((environ.get("AI","True")), False)
 
