448dde5711a129959428e778755488d7cd1358ef
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-28 11:01:23 +0530

    Update IFC.py

--- plugins/IFC.py
+++ plugins/IFC.py
@@ -19,31 +19,23 @@ async def ifsc_data(_, message: Message):
         ifsc_data = ifsc_data[1]
         m = await message.reply_text("Searching...")
     try:
-        url_request = req.get(API + query)
-        url_json = url_request.json()
-
-        # datas
-        swift = 'Swift :   ' + str(url_json['SWIFT']) + '\n'
-        city = 'City :   ' + str(url_json['CITY']) + '\n'
-        upi = 'UPI :   ' + str(url_json['UPI']) + '\n'
-        iso = 'ISO3166 :   ' + str(url_json['ISO3166']) + '\n'
-        neft = 'NEFT :   ' + str(url_json['NEFT']) + '\n'
-        imps = 'IMPS :   ' + str(url_json['IMPS']) + '\n'
-        rtgs = 'RTGS :   ' + str(url_json['RTGS']) + '\n'
-        centre = 'Centre :   ' + str(url_json['CENTRE']) + '\n'
-        address = 'Address :   ' + str(url_json['ADDRESS']) + '\n\n'
-        branch = 'Branch :   ' + str(url_json['BRANCH']) + '\n'
-        micr = 'MICR :   ' + str(url_json['MICR']) + '\n'
-        contact = 'Contact :   ' + str(url_json['CONTACT']) + '\n'
-        dist = 'District :   ' + str(url_json['DISTRICT']) + '\n'
-        state = 'State :   ' + str(url_json['STATE']) + '\n'
-        bank = 'Bank :   ' + str(url_json['BANK']) + '\n'
-        bankcd = 'Bank Code :   ' + str(url_json['BANKCODE']) + '\n'
-        ifsc = 'IFSC :   ' + str(url_json['IFSC']) + '\n'
-
-         result = head + '' + bank + bankcd + ifsc + micr + state + dist + city + branch + address + contact + upi + iso + neft + imps + rtgs + swift + ''
-
-        await m.edit_text(result, parse_mode=enums.ParseMode.HTML)
+        url = requests.get(f"https://ifsc.rizad.me/?ifsc={IFSC-CODE}")
+        response = json.loads(url.text)
+        text = f"""
+swift: {response['swift']}
+Status: {response['city']}
+upi: {response['upi']}
+neft: {response['neft']}
+iso : {response['iso']}
+rtgs: {response['rtgs']}
+imps : {response['imps']}
+centre: {response['centre']}
+address: {response['address']}
+branch: {response['branch']}
+dist: {response['District']}
+State: {response['state']}
+Bank: {response['Bank']}"""
+           await m.edit_text(result, parse_mode=enums.ParseMode.HTML)
     except Exception as e:
         await m.edit_text("Sorry ,'"+ifsc_data+"' is Invalid IFSC Code 😕")
         await m.edit_text("if you're facing a error or else ping @shado_hackers 🤝")
