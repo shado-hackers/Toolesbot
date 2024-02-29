02576f6f6bd52b73bf4a38b7e05537e84d8caacc
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-02-29 10:23:06 +0530

    Update IFC.py

--- plugins/IFC.py
+++ plugins/IFC.py
@@ -9,33 +9,36 @@ API='https://ifsc.razorpay.com/'
 head="Detailed InFo\n...................\n\n"
  
 
-@Client.on_message(filters.command("ifsc"))
-async def ifsc_data(_, message: Message):
-    ifsc_data = message.text.split(" ", 1)
-    if len(ifsc_data) == 1:
-        await message.reply_text("Usage:\n/ifsc [ifsc]")
-        return
-    else:
-        ifsc_data = ifsc_data[1]
-        m = await message.reply_text("Searching...")
-    try:
-        url = requests.get(f"https://ifsc.rizad.me/?ifsc={IFSC-CODE}")
-        response = json.loads(url.text)
-        text = f"""
-swift: {response['swift']}
-Status: {response['city']}
-upi: {response['upi']}
-neft: {response['neft']}
-iso : {response['iso']}
-rtgs: {response['rtgs']}
-imps : {response['imps']}
-centre: {response['centre']}
-address: {response['address']}
-branch: {response['branch']}
-dist: {response['District']}
-State: {response['state']}
-Bank: {response['Bank']}"""
-        await m.edit_text(text, parse_mode=enums.ParseMode.HTML)
-    except Exception as e:
-        await m.edit_text("Sorry ,'"+ifsc_data+"' is Invalid IFSC Code 😕")
-        await m.edit_text("if you're facing a error or else ping @shado_hackers 🤝")
+@app.on_message(filters.text)
+async def ifsc_data(client,message):
+    
+   query=message.text.upper()
+   try:
+    url_request=req.get(API+query)
+    url_json=url_request.json()
+    
+    #datas
+    swift='Swift :   `'+str(url_json['SWIFT'])+'`\n'
+    city='City :   `'+str(url_json['CITY'])+'`\n'
+    upi='UPI :   `'+str(url_json['UPI'])+'`\n'
+    iso='ISO3166 :   `'+str(url_json['ISO3166'])+'`\n'
+    neft='NEFT :   `'+str(url_json['NEFT'])+'`\n'
+    imps='IMPS :   `'+str(url_json['IMPS'])+'`\n'
+    rtgs='RTGS :   `'+str(url_json['RTGS'])+'`\n'
+    centre='Centre :   `'+str(url_json['CENTRE'])+'`\n'
+    address='Address :   `'+str(url_json['ADDRESS'])+'`\n\n'
+    branch='Branch :   `'+str(url_json['BRANCH'])+'`\n'
+    micr='MICR :   `'+str(url_json['MICR'])+'`\n'
+    contact='Contact :   `'+str(url_json['CONTACT'])+'`\n'
+    dist='District :   `'+str(url_json['DISTRICT'])+'`\n'
+    state='State :   `'+str(url_json['STATE'])+'`\n'
+    bank='Bank :   `'+str(url_json['BANK'])+'`\n'
+    bankcd='Bank Code :   `'+str(url_json['BANKCODE'])+'`\n'
+    ifsc='IFSC :   `'+str(url_json['IFSC'])+'`\n'
+    
+    result=head+'**__'+bank+bankcd+ifsc+micr+state+dist+city+branch+address+contact+upi+iso+neft+imps+rtgs+swift+'__**'
+        
+    await message.reply(result)
+   except:
+       await message.reply("**__Sorry ,`'"+query+"'` is Invalid IFSC Code 😕__**")
+       await message.reply("__if you're facing a error or else ping @riz4d 🤝__")
