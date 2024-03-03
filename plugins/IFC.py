8e162d69ff1c0913b50f5f1d0947e7fd4b6a49a3
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-03-03 17:50:55 +0530

    Update IFC.py

--- plugins/IFC.py
+++ plugins/IFC.py
@@ -1,44 +1,38 @@
+
 from pyrogram import Client
-import requests as req
-from plugins.info import head, ibase_url
-from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram import enums
 
-API='https://ifsc.razorpay.com/' 
-head="Detailed InFo\n...................\n\n"
- 
+API_URL = 'https://ifsc.razorpay.com/'
+INFO_HEADER = "Detailed Info\n...................\n\n"
+
 
 @Client.on_message(filters.text & filters.command(["ifsc"]))
-async def ifsc_data(client,message):
-    
-   query=message.from_user.id
-   try:
-    url_request=req.get(API+query)
-    url_json=url_request.json()
-    
-    #datas
-    swift='Swift :   `'+str(url_json['SWIFT'])+'`\n'
-    city='City :   `'+str(url_json['CITY'])+'`\n'
-    upi='UPI :   `'+str(url_json['UPI'])+'`\n'
-    iso='ISO3166 :   `'+str(url_json['ISO3166'])+'`\n'
-    neft='NEFT :   `'+str(url_json['NEFT'])+'`\n'
-    imps='IMPS :   `'+str(url_json['IMPS'])+'`\n'
-    rtgs='RTGS :   `'+str(url_json['RTGS'])+'`\n'
-    centre='Centre :   `'+str(url_json['CENTRE'])+'`\n'
-    address='Address :   `'+str(url_json['ADDRESS'])+'`\n\n'
-    branch='Branch :   `'+str(url_json['BRANCH'])+'`\n'
-    micr='MICR :   `'+str(url_json['MICR'])+'`\n'
-    contact='Contact :   `'+str(url_json['CONTACT'])+'`\n'
-    dist='District :   `'+str(url_json['DISTRICT'])+'`\n'
-    state='State :   `'+str(url_json['STATE'])+'`\n'
-    bank='Bank :   `'+str(url_json['BANK'])+'`\n'
-    bankcd='Bank Code :   `'+str(url_json['BANKCODE'])+'`\n'
-    ifsc='IFSC :   `'+str(url_json['IFSC'])+'`\n'
-    
-    result=head+'**__'+bank+bankcd+ifsc+micr+state+dist+city+branch+address+contact+upi+iso+neft+imps+rtgs+swift+'__**'
-        
-    await message.reply(result)
-   except:
-       await message.reply("**is Invalid IFSC Code 😕**")
-       
+async def ifsc_data(client: Client, message: Message):
+    query = message.from_user.id
+
+    try:
+        response = req.get(API_URL + query)
+        json_data = response.json()
+
+        info = INFO_HEADER
+        info += f"Bank: {json_data['BANK']}\n"
+        info += f"Bank Code: {json_data['BANKCODE']}\n"
+        info += f"IFSC: {json_data['IFSC']}\n"
+        info += f"MICR: {json_data['MICR']}\n"
+        info += f"State: {json_data['STATE']}\n"
+        info += f"District: {json_data['DISTRICT']}\n"
+        info += f"City: {json_data['CITY']}\n"
+        info += f"Branch: {json_data['BRANCH']}\n"
+        info += f"Address: {json_data['ADDRESS']}\n"
+        info += f"Contact: {json_data['CONTACT']}\n"
+        info += f"UPI: {json_data['UPI']}\n"
+        info += f"ISO3166: {json_data['ISO3166']}\n"
+        info += f"NEFT: {json_data['NEFT']}\n"
+        info += f"IMPS: {json_data['IMPS']}\n"
+        info += f"RTGS: {json_data['RTGS']}\n"
+        info += f"Swift: {json_data['SWIFT']}\n"
+
+        await message.reply(info)
+    except Exception as e:
+        await message.reply("Invalid IFSC Code 😕")
